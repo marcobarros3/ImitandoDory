1d0641537b86b5e1083a3a6778979941f076cc4f
ETEC Adolpho Berezin <barrosmarcobarros3@gmail.com>
2023-03-31 20:27:26 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("---ImitandoDry---");
+
+Console.WriteLine(" escreva uma frase...: ");
+String frase = Console.ReadLine()!;
+
+String fraseEmBaleies = frase
+.Replace ("a", "aaa")
+.Replace ("A", "AAA")
+.Replace ("e", "eee")
+.Replace ("E", "EEE")
+.Replace ("i", "iii")
+.Replace ("I", "III")
+.Replace ("o", "ooo")
+.Replace ("O", "OOO")
+.Replace ("u", "uuu")
+.Replace ("U", "UUU");
+
+Console.WriteLine($"\nEm Baleies:\n\n{fraseEmBaleies}");
